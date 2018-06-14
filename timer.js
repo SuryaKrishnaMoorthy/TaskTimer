a4dcdb0a81d5cb50dd7778670f51e9643d204135
SURYA KRISHNA MOORTHY <ayrus2289@gmail.com>
2018-06-14 15:51:31 -0700

    modified timer

--- timer.js
+++ timer.js
@@ -1,89 +1,111 @@
-/*To start the function as soon as page loads*/
+/* To start the function as soon as page loads */
 $(function() {
-  /*To extract parameters from the url*/
-  var queryString = new Array();
+    /* To extract parameters from the url */
+    let queryString = new Array();
 
-  if (queryString.length == 0) {
-    if (window.location.search.split('?').length > 1) {
+    if (queryString.length == 0) {
+        if (window.location.search.split('?').length > 1) {
 
-      /*splitting url and populating array 'params'*/
-      var params = window.location.search.split('?')[1].split('&');
+            /* Splitting url and populating array 'params' */
+            let params = window.location.search.split('?')[1].split('&');
 
-      /*Populating queryString array*/
-      for (var i = 0; i < params.length; i++) {
-        var key = params[i].split('=')[0];
-        var value = params[i].split('=')[1];
-        queryString[key] = value;
-      } //end for
-      console.log(queryString);
-    } //end if
-  } //end if
+            /* Populating queryString array */
+            for (let i = 0; i < params.length; i++) {
+                let key = params[i].split('=')[0];
+                let value = params[i].split('=')[1];
+                queryString[key] = value;
+            } // end for
+        } // end if
+    } // end if
 
-  /* Fetching the value from user input */
-  var givenMinutes = parseInt(queryString["minutes"]);
-  var givenSeconds = parseInt(queryString["seconds"]);
-  //
-  // if (isNaN(givenMinutes)) {
-  //   givenMinutes = 0;
-  // }
-  // if (isNaN(givenSeconds)) {
-  //   givenSeconds = 0;
-  // }
-
-  var startTime = Date.now();
-
-  /* Initilaizing local variables as per user input and Display the starting value of timer */
-  var seconds = givenSeconds;
-  var minutes = givenMinutes;
-  $("#time").text(display(minutes, seconds));
-
-  if (givenSeconds === 0) {
-    minutes--;
-    seconds = 59;
-  } else {
-    seconds--;
-  }
-
-  /* Main timer logic */
-  var intervalId = setInterval(function() {
-
-    /* Calculate the total seconds passed */
-    var currentTime = Date.now();
-    var secondsPast = Math.floor((currentTime - startTime) / 1000);
-    //console.log(secondsPast);
-    $("#time").text(display(minutes, seconds));
-    $("#timerTitle").text(display(minutes, seconds));
-
-    /* Decrement minutes when seconds approaches zero */
-    if (seconds === 0) {
-      minutes--;
-      seconds = 60;
-    }
-    seconds--;
-
-    /* Logic to stop the timer */
-    var totalSeconds = (givenMinutes * 60) + givenSeconds;
-    if (secondsPast === totalSeconds) {
-      $("#text").append("Timer Done!");
-      clearInterval(intervalId);
-    } //end if
-  }, 1000);
+    startTimer(queryString, "newTab");
 });
 
-/* function to add zero padding*/
+/* The main timer function */
+function startTimer(queryString, location) {
+
+    /* Fetching the value from user input */
+    let givenMinutes = parseInt(queryString["minutes"]);
+    let givenSeconds = parseInt(queryString["seconds"]);
+    let taskName = queryString["taskName"];
+
+    let startTime = Date.now();
+
+    /* Initilaizing local variables as per user input and Display the starting value of timer */
+    let seconds = givenSeconds;
+    let minutes = givenMinutes;
+    $(".taskName").text(taskName);
+    if (location === "newTab") {
+        $(".timeValue").text(display(minutes, seconds));
+    } else {
+        $(".timerValue").text(display(minutes, seconds));
+    }
+
+    if (givenSeconds === 0) {
+        minutes--;
+        seconds = 59;
+    } else {
+        seconds--;
+    }
+
+    /* Main timer logic */
+    if (givenMinutes || givenSeconds) {
+        let intervalId = setInterval(function() {
+
+            /* Calculate the total seconds passed */
+            let currentTime = Date.now();
+            let secondsPast = Math.floor((currentTime - startTime) / 1000);
+            if (location === "newTab") {
+                $(".timeValue").text(display(minutes, seconds));
+            } else {
+                $(".timerValue").text(display(minutes, seconds));
+            }
+            $(".timerTitle").text(display(minutes, seconds) + " - " + taskName);
+
+            /* Decrement minutes when seconds approaches zero */
+            if (seconds === 0) {
+                minutes--;
+                seconds = 60;
+            }
+            seconds--;
+
+            /* Logic to stop the timer */
+            let totalSeconds = (givenMinutes * 60) + givenSeconds;
+            if (secondsPast === totalSeconds) {
+                if (location === "newTab") {
+                    $(".textValue").append("Timer Done!");
+                } else {
+                    location = location.replace(/\s+/g, '.');
+                    $("li." + location).addClass("complete"); // mark the row complete
+                    $("button:contains('Started')").html("Start"); // revert the name
+                }
+                clearInterval(intervalId);
+            } //end if
+        }, 1000);
+        return intervalId;
+    }
+
+}
+
+/* Function to add zero padding*/
 function display(minutes, seconds) {
-  var mm;
-  var ss;
-  if (minutes < 10) {
-    mm = "0" + minutes + ":";
-  } else {
-    mm = minutes + ":";
-  }
-  if (seconds < 10) {
-    ss = "0" + seconds;
-  } else {
-    ss = seconds;
-  }
-  var str = mm + ss;
-  return str;
+    let mm;
+    let ss;
+    if (minutes < 10) {
+        mm = "0" + minutes + ":";
+    } else {
+        mm = minutes + ":";
+    }
+    if (seconds < 10) {
+        ss = "0" + seconds;
+    } else {
+        ss = seconds;
+    }
+    let str = mm + ss;
+    return str;
+}
+
+/* Change task name font color on mouse over */
+function changeFontColor(color) {
+    $("h3").css("color", color);
 }
--- todoList.js
+++ todoList.js
@@ -1,70 +1,136 @@
-/*Initializing variables*/
-var arr = [];
-var obj = {};
-var startBtn;
-var deleteBtn;
+/* Initializing letiables */
+let arr = [];
+let obj = {};
+let intervalId;
 
-/*Logic for add button*/
-$("#addBtn").click(function() {
-  if ($("#addTask").val().trim() &&
-    $("#addMinutes").val().trim() && $("#addSeconds").val().trim() &&
-    $("#addSeconds").val() <= 59 && $("#addSeconds").val() >= 0 &&
-    $("#addMinutes").val() >= 0) {
-    /*To populate array objects as key-value pairs*/
-    arr.push({
-      task: $("#addTask").val(),
-      min: $("#addMinutes").val(),
-      sec: $("#addSeconds").val()
-    });
-    /*Resetting values after each add task*/
-    $("#addTask").val("");
-    $("#addMinutes").val('5');
-    $("#addSeconds").val('0');
-    /*Loop through array*/
-    for (var i = 1; i <= arr.length; i++) {
+/* Logic for add button */
+$(".addButton").click(function() {
+    if ($(".addTask").val().trim() &&
+        $(".addMinutes").val().trim() && $(".addSeconds").val().trim() &&
+        $(".addSeconds").val() <= 59 && $(".addSeconds").val() >= 0 &&
+        $(".addMinutes").val() >= 0) {
 
-      var li = $("<li class='row" + i + "'>"); //to create list item with unique id
-      var taskAndTime = arr[i - 1]; //to get the 0th element
-
-      /*Accessing last element of array*/
-      if (i === (arr.length)) {
-
-        startBtn = $("<button id='StartButton' type = 'button'>Start Timer</button>");
-        deleteBtn = $("<button class='row" + i + " delTask' type='button'>Delete Task</button>");
-
-        /*To hide the deleted list item*/
-        deleteBtn.click(function() {
-          $(this).parent().hide();
+        /* To populate array objects as key-value pairs */
+        arr.push({
+            task: $(".addTask").val(),
+            min: $(".addMinutes").val(),
+            sec: $(".addSeconds").val()
         });
 
+        /* Resetting values after each add task */
+        $(".addTask").val("");
+        $(".addMinutes").val('5');
+        $(".addSeconds").val('0');
 
-        /*To navigate to new window with parameters*/
-        startBtn.click(function() {
-          if (!(li.hasClass("complete"))) {
-            var url = "/timer.html?minutes=" + taskAndTime.min + "&seconds=" + taskAndTime.sec;
-            window.open(url, '_blank');
-          } else {
-            alert("completed task!")
-            console.log(this.startBtn.parent().addClass('complete'));
-            //  console.log()
-          }
-        });
+        /* Loop through array */
+        for (let i = 1; i <= arr.length; i++) {
 
-        /*Display the list element with values*/
-        li.append("<span class= strike' id='task'>" + taskAndTime.task + "</span>	")
-          .append("<span class='strike' id='time'>" + taskAndTime.min + "min " + taskAndTime.sec + "sec " + "</span>	")
-          .append(startBtn).append(deleteBtn);
-        $("ol").append(li);
+            let li = $("<li class='row" + i +
+                "' title='Double click to mark the task completed/reset the timer.'>"); // to create list item
+            let taskAndTime = arr[i - 1]; // to get the 0th element
 
-      } //end if.
+            /* Accessing last element of array */
+            if (i === (arr.length)) {
+                let start = $("<button class='timerButton' type='button' title='Start timer'>" +
+                    "Start</button>");
+                let pause = $("<button class='timerButton' type='button' title='Pause timer'>" +
+                    "Pause</button>");
+                let stop = $("<button class='timerButton' type='button' title='Stop timer'>" +
+                    "Stop</button>");
+                let launch = $("<button class='timerButton' type='button' title='Launch timer in new tab'>" +
+                    "Launch</button>");
+                let remove = $("<button class='row" + i + " timerButton' type='button' title='Delete task'>" +
+                    "Delete</button>");
 
-      /*To strike of the completed tasks*/
-      li.dblclick(function() {
-        console.log(this);
-        $(this).toggleClass('complete');
-      });
-    } //end for
-  } else {
-    alert("You must enter valid task and time values.");
-  }
+                /* To hide the deleted list item */
+                remove.click(function() {
+                    $(this).parent().hide();
+                });
+
+                /* To navigate to new window with parameters */
+                launch.click(function() {
+                    if (!(li.hasClass("complete"))) {
+                        let url = "/timer.html?taskName=" + taskAndTime.task +
+                            "&minutes=" + taskAndTime.min + "&seconds=" + taskAndTime.sec;
+                        window.open(url, '_blank');
+                    } else {
+                        alert("Completed task!")
+                    }
+                });
+
+                /* To start the timer in the same page */
+                start.click(function() {
+                    if (!(li.hasClass("complete"))) {
+                        if (!(li.hasClass("started"))) {
+                            $('li').removeClass("started"); // remove any existing running timer row
+                            $(".timer").empty(); // clear the space of current timer
+                            clearInterval(intervalId);
+                            let timeValue = $("<h1 class='timerValue'></h1>");
+                            $(".timer").append(timeValue);
+                            let queryString = {
+                                "minutes": taskAndTime.min,
+                                "seconds": taskAndTime.sec
+                            };
+                            li.addClass("started"); // an identifier to know which row is active
+                            $("button:contains('Started')").html("Start");
+                            $("button:contains('Resume')").html("Pause");
+                            $(this).html("Started");
+                            intervalId = startTimer(queryString, li.attr("class"));
+                        }
+                    } else {
+                        alert("Completed task!")
+                    }
+                });
+
+                /* To pause the running timer */
+                pause.click(function() {
+                    if (li.hasClass("started")) {
+                        let title = $(this).text();
+                        if (title === "Pause") {
+                            clearInterval(intervalId);
+                            $(this).html("Resume");
+                        } else {
+                            let queryString = {
+                                "minutes": $(".timerValue").text().split(":")[0],
+                                "seconds": $(".timerValue").text().split(":")[1]
+                            };
+                            $(this).html("Pause");
+                            intervalId = startTimer(queryString, li.attr("class"));
+                        }
+                    }
+                });
+
+                /* To stop the time */
+                stop.click(function() {
+                    if (li.hasClass("started")) {
+                        $("button:contains('Started')").html("Start");
+                        $("button:contains('Resume')").html("Pause");
+                        $('li').removeClass("started");
+                        $(".timer").empty();
+                        clearInterval(intervalId);
+                    }
+                });
+
+                /* Display the list element with values */
+                li.append("<span class='taskList'>" + taskAndTime.task + "</span>")
+                    .append("<span class='timeList'>" + taskAndTime.min + "min " + taskAndTime.sec + "sec" + "</span>")
+                    .append(launch).append(remove).append(stop).append(pause).append(start);
+                if ($(".list").has("ol").length) {
+                    $("ol").append(li);
+                } else {
+                    let ol = $("<ol>");
+                    ol.append(li);
+                    $(".list").append(ol);
+                }
+
+            } // end if.
+
+            /* To strike of the completed tasks */
+            li.dblclick(function() {
+                $(this).toggleClass('complete');
+            });
+        } // end for
+    } else {
+        alert("You must enter valid task and time values.");
+    }
 });
